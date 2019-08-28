9158ecd86661e98ce5c0d481ff192d5c179e38eb
dev.yasinskiy <www@mail.cz>
2019-08-28 14:01:53 +0300

    init

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+// Route::get('listing/{listing}', function (Listing $listing) {
+//     return $listing->toJson();
+// });
+
+Route::get('/listing/{listing}', 'ListingController@get_listing_api');
