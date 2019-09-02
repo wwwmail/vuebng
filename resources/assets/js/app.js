51ed828fc0fd67a52022a487c4aabd75fcc74b99
dev.yasinskiy <www@mail.cz>
2019-09-02 10:45:58 +0300

    add .vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,42 +1,11 @@
-
 import Vue from 'vue';
+import ImageCarousel from '../components/ImageCarousel.vue';
+
+Vue.component('my-component', { template: '<div>My component!</div>' });
 
 
-Vue.component('my-component', {template: '<div>My component!</div>'});
 
-Vue.component('image-carousel', {
-    template: `<div class="image-carousel"> <img v-bind:src="image"/> 
-    <div class="controls"> 
-        <carousel-control dir="left"></carousel-control> <carousel-control dir="right"></carousel-control> 
-    </div>
-</div>`,
-    props: ['images'],
-    data() {
-        return {
-            index: 0
-        }
-    },
-    computed: {
-        image() {
-            return this.images[this.index];
-        }
-    },
-    components: {
-        'carousel-control': {
-            template: `<i :class="classes"></i>`,
-             props: [ 'dir' ],
-                computed: { 
-                    classes() { 
-                        return 'carousel-control fa fa-2x fa-chevron-' + this.dir; 
-                    } 
-                }
-                },
-               
-            }
-});
-
-
-import {populateAmenitiesAndPrices } from './helpers';
+import { populateAmenitiesAndPrices } from './helpers';
 let model = JSON.parse(window.vuebnb_listing_model);
 model = populateAmenitiesAndPrices(model);
 
@@ -44,11 +13,14 @@ model = populateAmenitiesAndPrices(model);
 
 var app = new Vue({
     el: "#app",
+    components:{
+        'image-carousel':ImageCarousel
+    },
     data: Object.assign(model, {
         contracted: true,
         modalOpen: false,
-        headerImageStyle: {"background-image": `url(${model.images[0]})`},
-        myval: false,
+        headerImageStyle: { "background-image": `url(${model.images[0]})` },
+        myval: false
     }),
 
     methods: {
@@ -60,7 +32,7 @@ var app = new Vue({
     },
 
     watch: {
-        modalOpen: function () {
+        modalOpen: function() {
             var className = "modal-open";
             if (this.modalOpen) {
                 document.body.classList.add(className);
@@ -69,10 +41,10 @@ var app = new Vue({
             }
         }
     },
-    created: function () {
+    created: function() {
         document.addEventListener("keyup", this.escapeKeyListener);
     },
-    destroyed: function () {
+    destroyed: function() {
         document.removeEventListener("keyup", this.escapeKeyListener);
     }
-});
+},);
